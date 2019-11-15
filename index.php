905eaaf1f0a44fcde4f2f3793d8d9a2d65ef4dcf
NDEYESOKHNA <sokhnatoure454@gmail.com>
2019-11-15 16:39:42 +0000

    changememt

--- index.php
+++ index.php
@@ -1,32 +1,93 @@
 <?php 
-/**
- * 
- */
-class Eleve 
+
+  
+/** 
+class Ordinateur
 {
-  public $nom;
-  public $prenom;
-  public $age;
-  public $numerocarte;
+  public $marque;
+  public $memoir;
+  public $disk;
 
-  public function __construct($def_nom , $def_prenom , $def_age , $def_numerocarte)
+  public function __construct($marque , $memoir , $disk )
   {
-    $this->nom =$def_nom;
-    $this->prenom = $def_prenom;
-    $this->age = $def_age;
-    $this->numerocarte = $def_numerocarte;
+    $this->marque=$marque;
+    $this->memoir = $memoir;
+    $this->disk = $disk;
+  
+ }
+ public function Affiche(){
+  return "$this->marque - $this->memoir  Go- $this->disk Terra";
+ }
+}
+class Laptop extends Ordinateur
+{
+  
+  public $tactile;
+  public $webcam;
+  public $batterie;
+  public function __construct($marque, $memoir ,$disk, $tactil,$webcam, $batterie)
+
+  
+  {
+  parent:: __construct($marque, $memoir ,$disk);
+  $this->tactil = $tactil;
+   $this->batterie = $batterie;
+    $this->webcam = $webcam;
   }
-
-  public function Afficheeleve()
-  {
-   echo "<h1>$this->nom</h1><h2>$this->prenom</h2><h3>$this->age</h3><h4>$this->numerocarte</h4>";
-
+  public function Affiche(){
+    return "$this->marque - $this->memoir Go- $this->disk Terra - $this->batterie";
   }
 }
-$elev= new Eleve('Toure' ,'Ndeye Sokhna' ,23 , "2016066A");
-$elev->Afficheeleve();
+
+  $Laptop = new Laptop("DELL", 4,1,true,true, "4H");
+   echo $Laptop->Affiche();
+  
+  */
+   class Vehicule{
+    public $genre;
+    public $nbroue;
+    public $matricul;
+    public $boite_vitesse;
+    public function __construct($genre, $nbroue, $matricul, $boite_vitesse){
+      $this->genre = $genre;
+      $this->nbroue = $nbroue;
+      $this->matricul = $matricul;
+      $this->boite_vitesse= $boite_vitesse;
 
 
+    }
+    public function Affiche(){
+     return "$this->genre-$this->nbroue -$this->matricul - $this->boite_vitesse " ;
+    }
+    public function Roule($vmax,$nbrs){
+      return "<h3>j'ai une vitesse maximal de $vmax /Heure en $nbrs second</h3>";
 
+    }
+   }
+   class Automobile extends Vehicule
+   {
+
+public $nbplace;
+public $signalisation;
+public function __construct($genre, $nbroue, $matricul, $boite_vitesse,$nbplace,$signalisation){
+  parent:: __construct($genre, $nbroue, $matricul, $boite_vitesse);
+  $this->nbplace = $nbplace;
+  $this->signalisation=$signalisation;
+}
+public function Roule($vmax,$nbrs){
+      return "<h3>j'ai une vitesse maximal de $vmax /Heure en $nbrs second</h3>";
+
+}
+
+
+   }
+ 
+$veh = new Vehicule("toyato" , 4 , "GT-5477", 24);
+echo $veh->Affiche();
+echo  $veh->Roule( 13,16);
+$au = new Automobile("jeep" , 4, 436, "true" , 45, "true");
+ echo $au->Affiche();
+ echo $au->Roule(23,87); 
  ?>
+  
 
